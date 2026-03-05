83d1cf4372a6bdd38ab98c227f4e57d46df62c27
palkamarlene <marlene.palka@zalf.de>
2026-03-05 09:37:28 +0100

    Write env

--- run-producer_calibration.py
+++ run-producer_calibration.py
@@ -642,8 +642,8 @@ def run_producer(server={"server": None, "port": None}):
                         climate_csv_path = (paths["monica-path-to-climate-dir"] +
                                             f"czechglobe/hist_csv_1961-01-01_to_2023-01-01/row-{crow}/col-{ccol}.csv.gz")
                         env_template["pathToClimateCSV"] = climate_csv_path
-                        #with open(path_to_out_file, "a") as _:
-                        #    _.write(f"{datetime.now()} pathToClimateCSV: {env_template['pathToClimateCSV']}\n")
+                        with open(path_to_out_file, "a") as _:
+                            _.write(f"{datetime.now()} pathToClimateCSV: {env_template['pathToClimateCSV']}\n")
 
                         env_template["customId"] = {
                             "setup_id": setup_id,
